a48774527695ffc7f7a2c87faf96cb4748f27a00
siimav <siim.aaver@gmail.com>
2020-10-22 02:23:26 +0300

    Fix serialization

--- RP1AnalyticsWebApp/Models/CareerLogDto.cs
+++ RP1AnalyticsWebApp/Models/CareerLogDto.cs
@@ -2,28 +2,28 @@
 {
     public class CareerLogDto
     {
-        public string careerUuid;
-        public string epoch;
-        public int vabUpgrades;
-        public int sphUpgrades;
-        public int rndUpgrades;
-        public double currentFunds;
-        public double currentSci;
-        public double scienceEarned;
-        public double advanceFunds;
-        public double rewardFunds;
-        public double failureFunds;
-        public double otherFundsEarned;
-        public double launchFees;
-        public double maintenanceFees;
-        public double toolingFees;
-        public double entryCosts;
-        public double constructionFees;
-        public double otherFees;
+        public string careerUuid { get; set; }
+        public string epoch { get; set; }
+        public int vabUpgrades { get; set; }
+        public int sphUpgrades { get; set; }
+        public int rndUpgrades { get; set; }
+        public double currentFunds { get; set; }
+        public double currentSci { get; set; }
+        public double scienceEarned { get; set; }
+        public double advanceFunds { get; set; }
+        public double rewardFunds { get; set; }
+        public double failureFunds { get; set; }
+        public double otherFundsEarned { get; set; }
+        public double launchFees { get; set; }
+        public double maintenanceFees { get; set; }
+        public double toolingFees { get; set; }
+        public double entryCosts { get; set; }
+        public double constructionFees { get; set; }
+        public double otherFees { get; set; }
 
-        public string[] launchedVessels;
-        public string[] contractEvents;
-        public string[] techEvents;
-        public string[] facilityConstructions;
+        public string[] launchedVessels { get; set; }
+        public string[] contractEvents { get; set; }
+        public string[] techEvents { get; set; }
+        public string[] facilityConstructions { get; set; }
     }
 }
