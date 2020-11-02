a8a359c5b54cd356aa7bc7cb66d6445a39a14a35
siimav <siim.aaver@gmail.com>
2020-11-03 01:54:51 +0200

    Add modal dialog with contract completions

---
+++ RP1AnalyticsWebApp/wwwroot/js/records.js
@@ -0,0 +1,24 @@
+﻿(() => {
+    const app = Vue.createApp(Contracts);
+    const vm = app.mount('#contracts');
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.modal');
+        var instances = M.Modal.init(elems);
+    });
+
+    window.getContracts = getContracts;
+
+    function getContracts(contractName) {
+        vm.contracts = null;
+        if (contractName) {
+            vm.contracts = null;
+            fetch(`/api/careerlogs/contracts/${contractName}`)
+                .then((res) => res.json())
+                .then((jsonContracts) => {
+                    vm.contracts = jsonContracts;
+                })
+                .catch((error) => alert(error));
+        }
+    }
+})();
