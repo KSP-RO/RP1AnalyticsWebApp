f68499d9ba00b09edd2fb9f78ef1f3e26475e5f3
Theysen <lukastheisen@outlook.de>
2021-03-27 11:18:35 +0100

    add new spinner animation

--- RP1AnalyticsWebApp/wwwroot/js/components/loading-spinner.js
+++ RP1AnalyticsWebApp/wwwroot/js/components/loading-spinner.js
@@ -1,14 +1,3 @@
 ﻿const LoadingSpinner = {
-    template: `
-        <div class="preloader-wrapper active">
-            <div class="spinner-layer spinner-blue-only">
-                <div class="circle-clipper left">
-                    <div class="circle"></div>
-                </div><div class="gap-patch">
-                    <div class="circle"></div>
-                </div><div class="circle-clipper right">
-                    <div class="circle"></div>
-                </div>
-            </div>
-        </div>`
+    template: `<div class="lds-ring"><div></div><div></div><div></div><div></div></div>`
 };
