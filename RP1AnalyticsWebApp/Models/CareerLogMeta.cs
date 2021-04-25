f825fcbf57074f51d5b66c2d77ce6bcd46512860
Theysen <lukastheisen@outlook.de>
2021-04-25 18:16:17 +0200

    use internal serialization for proper API output

--- RP1AnalyticsWebApp/Models/CareerLogMeta.cs
+++ RP1AnalyticsWebApp/Models/CareerLogMeta.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson;
+﻿using System.Text.Json.Serialization;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace RP1AnalyticsWebApp.Models
@@ -6,13 +7,22 @@ namespace RP1AnalyticsWebApp.Models
     [BsonIgnoreExtraElements]
     public class CareerLogMeta
     {
-        [BsonRepresentation(BsonType.String)] public CareerPlaystyle CareerPlaystyle { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [BsonRepresentation(BsonType.String)]
+        public CareerPlaystyle CareerPlaystyle { get; set; }
 
-        [BsonRepresentation(BsonType.String)] public DifficultyLevel DifficultyLevel { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [BsonRepresentation(BsonType.String)]
+        public DifficultyLevel DifficultyLevel { get; set; }
 
-        [BsonRepresentation(BsonType.String)] public ConfigurableStart ConfigurableStart { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [BsonRepresentation(BsonType.String)]
+        public ConfigurableStart ConfigurableStart { get; set; }
 
-        [BsonRepresentation(BsonType.String)] public FailureModel FailureModel { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [BsonRepresentation(BsonType.String)]
+        public FailureModel FailureModel { get; set; }
+
         public string DescriptionText { get; set; }
     }
 
@@ -27,7 +37,8 @@ namespace RP1AnalyticsWebApp.Models
     public enum DifficultyLevel
     {
         Easy,
-        Medium,
+        Normal,
+        Moderate,
         Hard
     }
 
