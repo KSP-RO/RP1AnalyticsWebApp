6d041065c372ce87db2eb85fd05d5cae6e502a08
siimav <siim.aaver@gmail.com>
2021-05-15 18:07:23 +0300

    DB migration code

--- RP1AnalyticsWebApp/Models/Settings/CareerLogDatabaseSettings.cs
+++ RP1AnalyticsWebApp/Models/Settings/CareerLogDatabaseSettings.cs
@@ -4,13 +4,17 @@
     {
         public string CareerLogsCollectionName { get; set; }
         public string ConnectionString { get; set; }
+        public string ConnectionString2 { get; set; }
         public string DatabaseName { get; set; }
+        public string DatabaseName2 { get; set; }
     }
 
     public interface ICareerLogDatabaseSettings
     {
         string CareerLogsCollectionName { get; set; }
         string ConnectionString { get; set; }
+        string ConnectionString2 { get; set; }
         string DatabaseName { get; set; }
+        string DatabaseName2 { get; set; }
     }
 }
