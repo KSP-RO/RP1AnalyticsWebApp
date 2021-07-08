e283127bc812434821ace4e89aa89aaccc79e230
siimav <siim.aaver@gmail.com>
2021-07-08 13:44:46 +0300

    Career meta fixes

--- RP1AnalyticsWebApp/wwwroot/js/components/meta-information.js
+++ RP1AnalyticsWebApp/wwwroot/js/components/meta-information.js
@@ -4,8 +4,11 @@
         isVisible() {
             return !!this.meta;
         },
+        versionFormatted() {
+            return !!this.meta.versionTag ? this.meta.versionTag : '?';
+        },
         dateFormatted() {
-            return this.meta.creationDate.split("T")[0];
+            return this.meta.creationDate ? this.meta.creationDate.split("T")[0] : '?';
         }
     },
 
@@ -18,36 +21,37 @@
                         <div class="title">{{meta.careerPlaystyle}}</div>
                         <div class="subtitle">Playstyle</div>
                 </div>
-                
+
                 <div class="column">
                         <div class="title">{{meta.difficultyLevel}}</div>
                         <div class="subtitle">Difficulty</div>
                 </div>
-        
+
                 <div class="column">
                         <div class="title">{{meta.failureModel}}</div>
                         <div class="subtitle">Failure Model</div>
                 </div>
-        </div>       
+        </div>
             <div class="columns is-vcentered is-centered has-text-centered">
                 <div class="column">
-                        <div class="title is-size-4">{{meta.versionTag}}</div>
+                        <div class="title is-size-4">{{versionFormatted}}</div>
                         <div class="subtitle is-size-6">RP-1 Version</div>
-                </div>                
+                </div>
                 <div class="column">
                         <div class="title is-size-4">{{meta.modRecency}}</div>
                         <div class="subtitle is-size-6">Recency</div>
-                </div>                
+                </div>
                 <div class="column">
                         <div class="title is-size-4">{{dateFormatted}}</div>
                         <div class="subtitle is-size-6">Creation Date</div>
-                </div>        
+                </div>
         </div>
-            
+
         <div class="pt-4 block">
             <div class="subtitle">Notes and Remarks</div>
                 <p>{{meta.descriptionText}}</p>
             </div>
         </div>
+    </div>
     `
 };
