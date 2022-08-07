2b6d23982db9642fb1eeb534c613acc85926598b
siimav <siim.aaver@gmail.com>
2022-08-07 22:11:42 +0300

    Add json information about current user to page

---
+++ RP1AnalyticsWebApp/Utilities/UserUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace RP1AnalyticsWebApp.Utilities
+{
+    public static class UserUtils
+    {
+        public static string[] GetRoles(this ClaimsPrincipal user)
+        {
+            IEnumerable<Claim> roleClaims = user.FindAll(ClaimTypes.Role);
+            return roleClaims.Select(r => r.Value).ToArray();
+        }
+    }
+}
